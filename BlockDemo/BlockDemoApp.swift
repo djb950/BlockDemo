dd5e43ccf3b83618ecd5c431ba57c3332ef2a6fa
djb950 <39559741+djb950@users.noreply.github.com>
2023-03-20 20:30:14 -0400

    Initial Commit

---
+++ BlockDemo/BlockDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockDemoApp.swift
+//  BlockDemo
+//
+//  Created by DJ A on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BlockDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
